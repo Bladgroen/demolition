e75acc7dfffb1f6a99190284c0efd931ffc7afa1
Bladgroen <67104310+Bladgroen@users.noreply.github.com>
2023-04-11 13:28:39 +0200

    🗃️ added indexeddb

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+import Dexie, { type Table} from 'dexie';
+
+export interface Friend {
+    id?: number;
+    name: string;
+    age: number;
+}
+
+export class MySubClassedDexie extends Dexie {
+
+    friends!: Table<Friend>;
+
+    constructor() {
+        super('myDatabase');
+        this.version(1).stores({
+            friends: '++id, name, age'
+        })
+    }
+}
+
+export const db = new MySubClassedDexie();
